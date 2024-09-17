e0ff9aaff435f184e5eb8e3e1ae4b8d185e506b0
linhmanhnguyen <linhmanhng@gmail.com>
2024-09-18 00:31:55 +0700

    fix

--- controller/myCart.js
+++ controller/myCart.js
@@ -1,5 +1,25 @@
 class MyCart {
 
+    static async getCartByCartID(req) {
+        try {
+            const gioHangID = '66e99f6d0388654415cebef3';
+            const url = `${CONSTANTS.NGROK_URL}/v1/sites/cart/?gioHangID=${gioHangID}`;
+            const response = await axios.get(url);
+            
+            return {
+                success: response.data.isSuccess,
+                data: response.data.data || [],
+            };
+        } catch (error) {
+            console.log(error); 
+            return {
+                success: false,
+                message: 'Đã có lỗi xảy ra, vui lòng thử lại!',
+                data: [],
+            };
+        }
+    }
+
     // ------- RENDER ------- //
 
     static async myCartRender(req, res) {
